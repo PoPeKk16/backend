efff77f5c7237c28054e506bc635649ee0c24e51
Piotrek <peter96@poczta.onet.pl>
2022-07-06 20:01:23 +0200

    Initial commit

---
+++ scratch.py
@@ -0,0 +1,9 @@
+# if False is False in [False]:
+#     print("Nie jestem falsy")
+# else:
+#     print("oj jestem, falsy :/")
+
+
+# x = False == False in [False]
+# x = False == False and False in [False]
+# print(x)
